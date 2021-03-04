1549df2d1d88471b58cb4a97f6a52d09fbfc2fa9
Danny McGee <dannymcgee@gmail.com>
2021-03-04 01:10:23 -0500

    Entry Point

--- hazel/src/lib.rs
+++ hazel/src/lib.rs
@@ -1,10 +1,36 @@
-pub fn print() {
-	println!("Hello, Hazel Engine!");
+use std::time::Instant;
+
+pub trait App {
+	fn run(&mut self, delta_time: f64);
 }
 
-#[cfg(test)]
-mod tests {
+struct Hazel<'a, T: App> {
+	last_tick: Instant,
+	app: &'a mut T,
+}
+impl<'a, T> Hazel<'a, T>
+where
+	T: App,
+{
+	fn new(app: &'a mut T) -> Hazel<'a, T> {
+		Hazel {
+			app,
+			last_tick: Instant::now(),
+		}
+	}
 
-	#[test]
-	fn it_works() {}
+	fn run(&mut self) {
+		loop {
+			let now = Instant::now();
+			let delta_time = (now - self.last_tick).as_nanos() as f64 / 1_000_000.0;
+
+			self.app.run(delta_time);
+
+			self.last_tick = now;
+		}
+	}
+}
+
+pub fn bootstrap<T: App>(app: &mut T) {
+	Hazel::new(app).run();
 }
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,11 @@
+struct SandboxApp;
+
+impl hazel::App for SandboxApp {
+	fn run(&mut self, delta_time: f64) {
+		println!("Tick! {}", delta_time);
+	}
+}
+
 fn main() {
-	hazel::print();
+	hazel::bootstrap(&mut SandboxApp);
 }
